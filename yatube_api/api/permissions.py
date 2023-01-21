3b8098c59b9ccb126d18c0d7eaf06f9012fb0547
Kobilkina <kobilkina@yandex.ru>
2023-01-21 12:06:02 +0300

    '6'

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -19,8 +19,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         return (request.method in permissions.SAFE_METHODS) or (
                 obj.author == request.user
         )
